3216f254610ef2d8a05f050528820608739ba4e5
Max Hluhov <holosievo@gmail.com>
2018-12-07 19:46:56 +0200

    updated 2 task

--- src/main/resources/requests/2.sql
+++ src/main/resources/requests/2.sql
@@ -1,9 +1,9 @@
-SELECT
-    PROJECT_NAME,
-    SUM(SALARY)
-FROM developers
-    LEFT JOIN project_developer ON developers.ID = project_developer.developer_id
-    LEFT JOIN projects ON project_id = projects.ID
-GROUP BY PROJECT_NAME;
-
-
+SELECT sum.PROJECT_NAME
+FROM (SELECT
+          PROJECT_NAME,
+          SUM(SALARY) AS SUM
+      FROM developers
+          LEFT JOIN project_developer ON developers.ID = project_developer.developer_id
+          LEFT JOIN projects ON project_id = projects.ID
+      GROUP BY PROJECT_NAME) sum
+GROUP BY sum.SUM DESC LIMIT 1;
--- src/main/resources/requests/5.sql
+++ src/main/resources/requests/5.sql
@@ -5,4 +5,4 @@ SELECT
 FROM customers, companies
   LEFT JOIN company_project ON companies.ID = company_project.company_id
   LEFT JOIN projects ON projects.ID = company_project.project_id
-GROUP BY COMPANY_NAME DESC ;
+GROUP BY COMPANY_NAME DESC LIMIT 1;
