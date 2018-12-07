cce27abb7934569efb45cf2115761cb7065d04e6
Max Hluhov <33693492+HluhovMax@users.noreply.github.com>
2018-12-07 19:51:15 +0200

    Delete alt_2.sql

--- src/main/resources/requests/alt_2.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  `projects`.`PROJECT_NAME`  AS 'PROJECT',
-  MIN(`developers`.`SALARY`) AS 'MIN_DEVELOPER_SALARY',
-  `developers`.`NAME`        AS 'DEVELOPER'
-FROM `developers`
-  LEFT JOIN `projects_developers` ON `developers`.`ID` = `projects_developers`.`DEVELOPER_ID`
-  LEFT JOIN `projects` ON `projects_developers`.`PROJECT_ID` = `projects`.`ID`;
